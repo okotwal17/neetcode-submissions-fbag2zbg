55acae9f8f59a994f1ce81a2678338331bb7310d
Om Kotwal <omkotwal1811@gmail.com>
2026-04-24 12:37:22 -0500

    Add: course-schedule-ii - submission-4

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-4.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adjacencyList = {i : [] for i in range(numCourses)}
+        for c1, c2 in prerequisites:
+            adjacencyList[c1].append(c2)
+        visited = set()
+        cycle = set()
+        path = []
+        def dfs(crs):
+            if crs in cycle:
+                return False
+            if crs in visited:
+                return True
+            cycle.add(crs)
+            visited.add(crs)
+            for c in adjacencyList[crs]:
+                if not dfs(c):
+                    return False
+            path.append(crs)
+            cycle.remove(crs)
+            return True
+        for i in range(numCourses):
+            if not dfs(i):
+                return []
+        return path
