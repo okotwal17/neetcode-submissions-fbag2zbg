873bd609d0e98de74a4b2f4b258afc308af7d9fe
Om Kotwal <omkotwal1811@gmail.com>
2026-05-09 12:42:44 -0500

    Add: hand-of-straights - submission-0

---
+++ Data Structures & Algorithms/hand-of-straights/submission-0.py
@@ -0,0 +1,18 @@
+from collections import Counter
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        hashmap = Counter(hand)
+        startSize = len(hand)
+        hand.sort()
+        for card in hand:
+            if hashmap[card] != 0:
+                for i in range(groupSize):
+                    if not hashmap[card+i]:
+                        return False
+                    elif hashmap[card+i] > 0:
+                        startSize-=1
+                        hashmap[card+i]-=1
+        return True if startSize == 0 else Fasle
+                    
+
+                
