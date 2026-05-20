fa629eb085cf14926ccd6b49144e0eedfc73794b
Om Kotwal <omkotwal1811@gmail.com>
2026-05-19 19:55:16 -0500

    Add: count-paths - submission-1

---
+++ Data Structures & Algorithms/count-paths/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        grid = []
+        for i in range(m):
+            tempList = []
+            for j in range(n):
+                tempList.append(1)
+            grid.append(tempList)
+        for i in range(1, m):
+            for j in range(1, n):
+                grid[i][j] = grid[i - 1][j] + grid[i][j-1]
+        return grid[m-1][n-1]
+
+            
