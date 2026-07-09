57365ca24d6380bb48e3b827002ef4753b77aade
Om Kotwal <omkotwal1811@gmail.com>
2026-07-08 19:03:34 -0500

    Add: merge-k-sorted-linked-lists - submission-6

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-6.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        heap = []
+        #Adding to the heap
+        for i,node in enumerate(lists):
+            if node:
+                heapq.heappush(heap, (node.val, i, node))
+        dummy = ListNode(-1)
+        cur = dummy
+        while heap:
+            val, idx, node = heapq.heappop(heap)
+            cur.next = node
+            cur = cur.next
+            if node.next:
+                heapq.heappush(heap, (node.next.val, idx, node.next))
+        return dummy.next
