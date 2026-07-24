5a849481cd50235e82ef25fee90c28037098e903
Om Kotwal <omkotwal1811@gmail.com>
2026-07-24 18:26:09 -0500

    Add: min-cost-climbing-stairs - submission-8

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-8.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        cost.append(0)
+        top, bottom = 0, cost[-1]
+        for i in range(len(cost) - 2, -1, -1):
+            top, bottom = bottom, cost[i] + min(top, bottom)
+        return min(top, bottom)
+        
