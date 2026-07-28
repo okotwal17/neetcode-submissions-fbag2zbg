e08d654925fcda51938dc878305964cd46177a73
Om Kotwal <omkotwal1811@gmail.com>
2026-07-27 19:24:41 -0500

    Add: surrounded-regions - submission-5

---
+++ Data Structures & Algorithms/surrounded-regions/submission-5.py
@@ -0,0 +1,22 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        ROWS, COLS = len(board), len(board[0])
+        directions = [[1,0],[0,1],[-1,0],[0,-1]]
+        visited = set()
+        def dfs(r, c):
+            if r < 0 or c < 0 or r == ROWS or c == COLS or (r, c) in visited or board[r][c] == "X":
+                return
+            visited.add((r,c))
+            for dr, dc in directions:
+                dfs(r + dr, c + dc)
+        for i in range(COLS):
+            dfs(0, i)
+            dfs(ROWS - 1, i)
+        for i in range(ROWS):
+            dfs(i, 0)
+            dfs(i, COLS - 1)
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "O" and (r, c) not in visited:
+                    board[r][c] = "X"
+        
