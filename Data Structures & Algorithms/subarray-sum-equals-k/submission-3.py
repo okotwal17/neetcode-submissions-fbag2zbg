a011a0e468d5dde736f5bbb51afcf099b61e148b
Om Kotwal <omkotwal1811@gmail.com>
2026-08-18 12:12:25 -0500

    Add: subarray-sum-equals-k - submission-3

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        #Use the hashamp to see if there is a chopped off bit in there.  
+        res = 0
+        hashmap = {0 : 1}
+        prefixSum = 0
+        for num in nums:
+            prefixSum += num
+            target = prefixSum - k
+            if target in hashmap:
+                res += hashmap[target]
+            hashmap[prefixSum] = hashmap.get(prefixSum, 0) + 1
+        return res
