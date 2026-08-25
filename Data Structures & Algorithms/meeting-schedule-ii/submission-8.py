2e0ac6acce4337a7aa501fc6fe787e0c61b38529
Om Kotwal <omkotwal1811@gmail.com>
2026-08-24 20:23:00 -0500

    Add: meeting-schedule-ii - submission-8

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-8.py
@@ -0,0 +1,23 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        start = sorted([x.start for x in intervals])
+        end = sorted([x.end for x in intervals])
+        res, count = 0, 0
+        s, e = 0, 0
+        while s < len(start) and e < len(end):
+            if start[s] < end[e]:
+                count += 1
+                s += 1
+            else:
+                e += 1
+                count -= 1
+            res = max(count, res)
+        return res
